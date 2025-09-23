67cb9009f0a68323bab9fc2ed0a1b47485e3abb8
Kolipaka Akhil Surya <130474386+AkhilSuryaK@users.noreply.github.com>
2025-09-23 21:05:15 +0530

    50848

--- manage.py
+++ manage.py
@@ -7,10 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_management.settings')
-    if 5>10:
-        print("Impossible")
-    else:
-        print("Hii Hello")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
