04384dae0f3f606e858b4a9c7309a98d588bbf41
Kolipaka Akhil Surya <130474386+AkhilSuryaK@users.noreply.github.com>
2025-09-23 21:02:38 +0530

    27216

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ def main():
     if 5>10:
         print("Impossible")
     else:
-        print("Hiii")
+        print("Hii")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
