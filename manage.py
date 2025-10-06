e75485b79be6b2727a5bff9c51d740cb413d83fb
Kolipaka Akhil Surya <130474386+AkhilSuryaK@users.noreply.github.com>
2025-10-06 17:34:40 +0530

    53889

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_management.settings')
-    print("12")
+    print("123")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
